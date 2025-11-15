cf0b4f8ba2a641a7ce4147fa537fa4e9397e1282
Kimdabin <shekqs11004@gmail.com>
2025-11-16 05:46:29 +0900

    Lab11 completed

---
+++ tester.py
@@ -0,0 +1,11 @@
+import geo.utils as utils
+
+# hypotenuse of a=3, b=4
+a, b = 3, 4
+c = utils.pythagoras(a, b)
+print("c =", c)
+
+# circle area when r=10
+r = 10
+area = utils.circle(r)
+print("area =", area)
